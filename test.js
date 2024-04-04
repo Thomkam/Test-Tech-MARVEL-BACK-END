9bca6a3198064fb35959fde5935a8ba42139118a
Thomkam <thomas-kamoun@hotmail.fr>
2024-04-04 19:53:11 +0200

    first commit

---
+++ test.js
@@ -0,0 +1,37 @@
+// pour north flank (hebergement su server en ligne) => ne pas oublier le script START dans le package.json
+// dans north flank ne pas oublié l'environnement (si .env) avant de build
+//ne pas mettre le PORT en dure dans notre code
+
+//pour netlify ne pas oublier de creer le dossier_redirect
+
+//---------
+
+//mettre son api key via postman
+
+//-----------
+//les packages du back
+
+//npm init -y
+//installer express et cors
+//axios
+
+//mongoose si on fait les bonus
+
+//-----------
+//création de fichier index.js
+//et mettre les appel des packages
+
+//----------
+//ionstaller le package dotenv et importer le package puis cacher l'apikey dans un fichier qu'on cré et .env avec dedans genre API_KEY..... et changer le port avec PORT(et process.env.PORT et process.env.API_KEY)
+
+//faire sur toute les route du back la mm logique pour apres dans le front
+
+///-------------
+//pour le front
+//installer react router dom (cf cour pagination)
+//creer dossier pages
+// dans app.jsx importer router et importer le composant
+//creer commposent (ex: character), installer puis importer axios, ouis cr&ation du use effect avec import de use effect
+//importer aussi useState dans la page character
+// dans le use effect faire le fetch data et faire le trycatch
+// puis création
